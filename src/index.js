ec357082c0dda034570f27eb61bd2d5414e0ecd0
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-03-27 02:10:05 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: './config/.env' });
 const routes = require('./routes/routes.js');
 
-
 const app = express();
+
+app.use(express.json());
+
 
 
 app.use('/api', routes);
-mongoose.connect(process.env.MONGODB_CONNECT_URL, {
-
-});
+mongoose.connect(process.env.MONGODB_CONNECT_URL);
 
 app.listen(3000, () => {
     console.log('Servidor em execução na porta 3000');
