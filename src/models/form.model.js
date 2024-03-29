3ac9c6b2fda62903cf311b34cc469cf3ca6fb72e
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-03-29 19:36:23 -0300

    -

--- src/models/form.model.js
+++ src/models/form.model.js
@@ -32,13 +32,55 @@ const FormSchema = new mongoose.Schema({
 
       next()
   });
-  FormSchema.post('findOne', function(doc, next) {
+  FormSchema.post('findOne', async function(doc, next) {
     if (doc) {
+        // Descriptografa o título e a descrição do formulário
         doc.title = decrypt(doc.title);
         doc.description = decrypt(doc.description);
+
+        // Descriptografa as perguntas individuais
+        await Promise.all(doc.questions.map(async question => {
+            question.title = decrypt(question.title);
+            question.description = decrypt(question.description);
+            if (question.options && Array.isArray(question.options)) {
+                question.options = question.options.map(option => decrypt(option));
+            }
+            return question;
+        }));
     }
     next();
 });
+
+// FormSchema.post('find', async function(docs, next) {
+//     if (docs) {
+//         // Descriptografa o título e a descrição de cada formulário
+//         await Promise.all(docs.map(async doc => {
+//             doc.title = decrypt(doc.title);
+//             doc.description = decrypt(doc.description);
+
+//             // Descriptografa as perguntas individuais de cada formulário
+//             await Promise.all(doc.questions.map(async question => {
+//                 question.title = decrypt(question.title);
+//                 question.description = decrypt(question.description);
+//                 if (question.options && Array.isArray(question.options)) {
+//                     question.options = question.options.map(option => decrypt(option));
+//                 }
+//                 return question;
+//             }));
+//             return doc;
+//         }));
+//     }
+//     next();
+// });
+
+//   FormSchema.post('findOne', function(doc, next) {
+//     if (doc) {
+//         doc.title = decrypt(doc.title);
+//         doc.description = decrypt(doc.description);
+        
+//     }
+//     next();
+// });
 
 FormSchema.post('find', function(docs, next) {
     if (docs) {
